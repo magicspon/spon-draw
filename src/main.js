734e93861eba1904d0bb304495ae358b09554b95
Dave Stockley <leon@ournameismud.co.uk>
2018-02-02 15:56:23 +0000

    smaller

--- src/main.js
+++ src/main.js
@@ -1,5 +1,44 @@
 import mitt from 'mitt'
-import { animationEnd, eventPromise } from './utils'
+
+const eventPromise = (event, element, callback) => {
+	let complete = false
+
+	const done = (resolve, e) => {
+		e.stopPropagation()
+		element.removeEventListener(event, done)
+		if (e.target === element && !complete) {
+			complete = true
+			resolve()
+			return
+		}
+	}
+
+	return new Promise(resolve => {
+		callback && callback()
+		element.addEventListener(event, done.bind(null, resolve), false)
+	})
+}
+
+const animationEnd = (type = 'transition') => {
+	let types =
+		type === 'transition'
+			? {
+					OTransition: 'oTransitionEnd',
+					WebkitTransition: 'webkitTransitionEnd',
+					MozTransition: 'transitionend',
+					transition: 'transitionend'
+				}
+			: {
+					OAnimation: 'oAnimationEnd',
+					WebkitAnimation: 'webkitAnimationEnd',
+					MozAnimation: 'animationend',
+					animation: 'animationend'
+				}
+	const elem = document.createElement('fake')
+	return Object.keys(types).reduce(function(prev, trans) {
+		return undefined !== elem.style[trans] ? types[trans] : prev
+	}, '')
+}
 
 /**
  * @class SponDraw
